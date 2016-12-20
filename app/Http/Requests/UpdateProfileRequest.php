0f8b826b5ade594f1b7ac3a1b89e46f16a6c4fb6
Latif Sulistyo <latifsulistyo.me@gmail.com>
2016-12-20 23:29:03 +0700

    fix typo

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -7,7 +7,7 @@ class UpdateProfileRequest extends StoreProfileRequest
     public function rules()
     {
         $rules = parent::rules();
-        $rules['phone'] = 'required|unique:profiles,phone' . $this->route('profile');
+        $rules['phone'] = 'required|unique:profiles,phone,' . $this->route('profile');
         return $rules;
     }
 }
